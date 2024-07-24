46f8c02a5c55493e8ac3e6fd6985b0ccb1d339e8
abigailoasare <153215531+abigailoasare@users.noreply.github.com>
2024-07-24 15:35:22 +0200

    Add files via upload

---
+++ Rcodes/02_dhs_gps_prep/02_dhsgps_01-varGE.R
@@ -0,0 +1,232 @@
+# Load packages ----------------------------------------------------------------
+library(haven)
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+library(DescTools)
+library(data.table)
+library(Hmisc)
+library(labelled)
+library(sjlabelled)
+library(expss)
+library(naniar)
+#load geo_packages
+library("sf")
+library("sp")
+library("rgdal")
+library("raster") # For Raster Data
+library("rgeos")
+library("tidyverse")
+library("ncdf4")
+library("ggspatial")
+library("parallel")
+library("data.table")
+library("overviewR")
+library(kableExtra)
+library(gt)
+library(flextable)
+library(pastecs)
+library(Hmisc)
+library("huxtable")
+library(mapedit)
+library(tmap)
+library(mapview)
+
+# Setup -------------------------------------------------------------------
+source("./00_setup.R")
+
+
+# Load overview data -----------------------------------------------------------
+
+overview <- read_dta(file.path(dir[["gps_raw"]] , "overview.dta"))
+
+overview <- overview %>%
+  filter(datacategory == "GIS", dcode == "GE")
+
+ccode <- c(overview$ccode)
+version_GE <- c(overview$version_GE)
+
+variables <-
+  c(
+    "OBJECTID",
+    "DHSID",
+    "DHSCC" ,
+    "DHSYEAR",
+    "DHSCLUST",
+    "CCFIPS",
+    "ADM1FIPS",
+    "ADM1FIPSNA",
+    "ADM1SALBNA",
+    "ADM1SALBCO",
+    "ADM1DHS",
+    "ADM1NAME" ,
+    "DHSREGCO",
+    "DHSREGNA",
+    "SOURCE" ,
+    "URBAN_RURA",
+    "LATNUM",
+    "LONGNUM",
+    "ALT_GPS",
+    "ALT_DEM",
+    "DATUM",
+    "geometry",
+    "iso2code",
+    "version_GE"
+  )
+
+# Separating Shape  files ------------------------------------------------------
+
+shp_files <-
+  list.files(
+    dir[["gps_raw"]] ,
+    pattern = ".shp$",
+    recursive = T,
+    ignore.case = T
+  ) %>%
+  as_tibble() %>%
+  #dplyr::filter(value != "*.shp.xml") %>%
+  dplyr::mutate(
+    version_GE = stringr::str_sub(value, -8, -7),
+    type = stringr::str_sub(value, -10, -9),
+    ccode = stringr::str_sub(value, -12, -11)
+  )
+
+
+
+list_1 <-
+  c(
+    "AO",
+    "BJ",
+    "BT",
+    "MZ",
+    "BF",
+    "BU",
+    "CM",
+    "CV",
+    "CF",
+    "CD",
+    "TD",
+    "CG",
+    "CI",
+    "ST",
+    "EG",
+    "SL",
+    "EK",
+    "ER",
+    "ET",
+    "GA",
+    "GM",
+    "GH",
+    "GN",
+    "TG",
+    "TN",
+    "UG",
+    "KE",
+    "KM",
+    "LS",
+    "LB",
+    "MD",
+    "ML",
+    "MW",
+    "MA",
+    "NI",
+    "NG",
+    "MZ",
+    "OS",
+    "RW",
+    "NM",
+    "RW",
+    "SN",
+    "ZA",
+    "SD",
+    "SZ",
+    "TZ",
+    "ZM",
+    "ZW"
+  )
+
+
+list_shp <- shp_files %>%
+  dplyr::filter(ccode %in% list_1) %>%
+  dplyr::select(value) %>%
+  as.list() %>%
+  unlist()
+
+
+
+## Loop across all countries and Versions for shape files-----------------------
+
+out_shapefile <- lapply(list_shp, function(x) {
+  file <- sf::read_sf(paste(dir[["gps_raw"]], x, sep = "/")) %>%
+    sf::st_transform(4326) %>%  # Transform to WGS84 (EPSG:4326)
+    as_tibble()
+  
+  for (f in variables[!(variables %in% names(file))]) {
+    #print(f)
+    f <- stringr::str_trim(f, side = "both")
+    file <- file %>%
+      dplyr::mutate(!!f := NA_real_)
+  }
+  
+  file <- file %>%
+    dplyr::select(DHSID,
+                  DHSCC,
+                  DHSYEAR,
+                  DHSCLUST,
+                  CCFIPS,
+                  LATNUM,
+                  LONGNUM,
+                  geometry) %>%
+    dplyr::filter(!LATNUM == 0 & !LONGNUM == 0) %>%
+    mutate(# Add Information on Country and Version/Phase
+      iso2code = str_sub(x, 1, 2),
+      version_GE = str_sub(x, 5, 6))
+  
+  
+  return(file)
+})
+
+
+## Appending all countries and versions ----------------------------------------
+full_gps_shp <- out_shapefile %>%
+  bind_rows()
+
+
+## Saving dhs_GPS_full Data ----------------------------------------------------
+
+
+dhs_clusters <- full_gps_shp %>%
+  dplyr::select(DHSID,
+                DHSCC,
+                DHSYEAR,
+                DHSCLUST,
+                LATNUM,
+                LONGNUM,
+                geometry,
+                iso2code,
+                version_GE) %>%
+  dplyr::mutate(dhs_id = row_number())
+
+
+
+## Export as CSV and shapefile -------------------------------------------------
+
+st_write(
+  dhs_clusters,
+  dsn = file.path(dir[["data-processed"]],"dhs_clusters.shp"),
+  layer = "dhs_clusters.shp",
+  driver = "ESRI Shapefile",
+  append = FALSE
+)
+
+write_csv(dhs_clusters, file.path(dir[["data-processed"]], "dhs_clusters.csv"))
+
+rm(list = ls())
+
+
+
+
+
+
+
+
